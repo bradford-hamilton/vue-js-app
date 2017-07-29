3fdde1f1d556e9f8a7f30d7f11fcb403ccd2bb9f
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-07-28 19:32:35 -0600

    SweetPage.vue

--- src/components/SweetPage.vue
+++ src/components/SweetPage.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="sweet-page">
     <h1>{{ message }}</h1>
+    <button @click="handleButtonClick">Click this to toggle some silly little pokemons</button>
+    <ul>
+      <li v-for="pokemon in pokemons" v-if="show">
+        {{pokemon.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'sweetpage',
+  mounted() {
+    this.populatePokemon();
+  },
+  methods: {
+    populatePokemon() {
+      // what to use here for http request?
+      axios('http://pokeapi.co/api/v2/pokemon')
+        .then((data) => {
+          this.pokemons = data.data.results;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    handleButtonClick() {
+      this.show = !this.show;
+    },
+  },
   data() {
     return {
       message: 'This page is so sweet right?',
+      pokemons: [],
+      show: false,
     };
   },
 };
